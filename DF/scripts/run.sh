789b3c8ec51d9748d6aeded1d97703b6caec7025
feldman <feldman4@mit.edu>
2021-09-11 16:11:24 +0200

    now installed

--- DF/scripts/run.sh
+++ DF/scripts/run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-home=DF
-PYTHONPATH=$home/src:$PYTHONPATH python $home/src/game/app.py $@
+# home=DF
+# PYTHONPATH=$home/src:$PYTHONPATH python $home/src/game/app.py $@
+python game/app.py $@
