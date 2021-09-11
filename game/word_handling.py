7a2b1a9b1ae05c3ca71b96d349faf0981c44c66e
Alex <s6alhoch@uni-bonn.de>
2021-09-11 17:31:17 +0200

    Simple scoreing function is inplace

---
+++ game/word_handling.py
@@ -0,0 +1,20 @@
+import gensim
+import math
+
+
+def score_words(attack_word: str, defense_word: str, model: gensim.models.keyedvectors.KeyedVectors) -> float:
+    """Calculates similarity between the two input words, or an input sentence and a word based on the input model"""
+
+    # Adjust words to lowercase
+    defe = defense_word.lower()
+    attk = attack_word.lower().replace('-', ' ').split()
+
+    # Calculate similarity
+    sim = model.similarity(attk, defe)
+
+    # Calculate global word ranks
+    ranks = [1 - (model.rank(a, defe) / len(model)) for a in attk]
+    mean_ranks = (sum(ranks) * 100 / len(ranks))
+
+    # Return rounded rank weighted similarity
+    return round(abs((sum(sim) / len(sim)) * math.log10(mean_ranks)), 3)
