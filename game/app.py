78bd56bdce75f1b8428619b92c1e15b0248d863c
feldman <feldman4@mit.edu>
2021-09-11 17:03:39 +0200

    download vocab

--- game/app.py
+++ game/app.py
@@ -12,5 +12,14 @@ def server():
     server.run()
 
 
+def update_vocab():
+    import pandas as pd
+    local = 'resources/vocab.csv'
+
+    sheet_id = '1lkxMe_MYYsi8ecTe-otTqaHLAuM0Mu2ALTNaIzZANrI'
+    sheet_name = 'grammars'
+    url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}'
+    pd.read_csv(url).to_csv(local, index=None)
+
 if __name__ == '__main__':
     fire.Fire()
