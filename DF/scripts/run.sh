a826774c8280ad340b72d425f084fe7244e15364
feldman <feldman4@mit.edu>
2021-09-12 10:08:24 +0200

    logging

--- DF/scripts/run.sh
+++ DF/scripts/run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # home=DF
 # PYTHONPATH=$home/src:$PYTHONPATH python $home/src/game/app.py $@
+mkdir -p logs
 python game/app.py $@ 1> logs/server.out
--- DF/scripts/server.sh
+++ DF/scripts/server.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+mkdir -p logs
 python game/app.py server 1> logs/server.out
