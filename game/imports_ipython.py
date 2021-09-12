3da5b0eae151cf3cbc6a88aaac4cdb908aa7e33a
feldman <feldman4@mit.edu>
2021-09-12 17:07:01 +0200

    Update imports_ipython.py

--- game/imports_ipython.py
+++ game/imports_ipython.py
@@ -6,3 +6,8 @@ IPython.get_ipython().run_line_magic('matplotlib', 'inline')
 
 from game import app, term, server
 from game.constants import *
+
+import re
+from glob import glob
+import pandas as pd
+import numpy as np
